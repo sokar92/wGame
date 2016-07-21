3bce03f5beaad147462cd294e5fa3c45f4a39b29
Patryk Mikos <patryk912@gmail.com>
2016-07-21 14:55:19 +0200

    Fraction / Math

---
+++ wGame/Core/Fraction.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+namespace wGame.Core
+{
+    public class Fraction : IEquatable<Fraction>, IComparable, IComparable<Fraction>
+    {
+        private long Counter { get; set; }
+        private long Denominator { get; set; }
+
+        public bool IsNaN
+        {
+            get { return Counter == 0L && Denominator == 0L; }
+        }
+
+        public bool IsInfinity
+        {
+            get { return Counter != 0L && Denominator == 0L; }
+        }
+
+        public bool IsPositive
+        {
+            get { return Counter > 0L; }
+        }
+
+        public bool IsNegative
+        {
+            get { return Counter < 0L; }
+        }
+
+        public bool IsPositiveInfinity
+        {
+            get { return IsInfinity && IsPositive; }
+        }
+
+        public bool IsNegativeInfinity
+        {
+            get { return IsInfinity && IsNegative; }
+        }
+
+        public bool IsZero
+        {
+            get { return Counter == 0L && Denominator != 0L; }
+        }
+        
+        public Fraction(long l)
+        {
+            Counter = l;
+            Denominator = 1L;
+        }
+
+        public Fraction(long counter, long denominator)
+        {
+            if (denominator == 0L)
+            {
+                Denominator = 0L;
+                if (counter > 0L) Counter = 1L;
+                if (counter < 0L) Counter = -1L;
+                if (counter == 0L) Counter = 0L;
+            }
+            else
+            {
+                if (denominator < 0L)
+                {
+                    counter = -counter;
+                    denominator = -denominator;
+                }
+
+                var gcd = Math.Gcd(counter < 0L ? -counter : counter, denominator);
+                Counter = counter / gcd;
+                Denominator = denominator / gcd;
+            }
+        }
+
+        public static implicit operator Fraction(long l)
+        {
+            return new Fraction(l);
+        }
+
+        public static Fraction operator -(Fraction a)
+        {
+            return new Fraction(-a.Counter, a.Denominator);
+        }
+
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            return new Fraction(a.Counter*b.Denominator + a.Denominator*b.Counter, a.Denominator*b.Denominator);
+        }
+
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            return a + (-b);
+        }
+
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            return new Fraction(a.Counter*b.Counter, a.Denominator*b.Denominator);
+        }
+
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            return new Fraction(a.Counter*b.Denominator, a.Denominator*b.Counter);
+        }
+
+        public int CompareTo(Fraction other)
+        {
+            if (ReferenceEquals(other, null))
+                return +1;
+
+            // a/b < c/d => ad < cb
+            var ad = Counter * other.Denominator;
+            var cb = Denominator * other.Counter;
+
+            if (ad == cb) return 0;
+            return ad < cb ? -1 : +1;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return CompareTo(obj as Fraction);
+        }
+
+        public bool Equals(Fraction other)
+        {
+            return this == other;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var frac = obj as Fraction;
+            return !ReferenceEquals(frac, null) && Equals(frac);
+        }
+
+        public override int GetHashCode()
+        {
+            return Counter.GetHashCode() ^ Denominator.GetHashCode();
+        }
+
+        public static bool operator ==(Fraction a, Fraction b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null)) return true;
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null)) return false;
+            return a.Counter == b.Counter && a.Denominator == b.Denominator;
+        }
+
+        public static bool operator !=(Fraction a, Fraction b)
+        {
+            return !(a == b);
+        }
+
+        public static bool operator <=(Fraction a, Fraction b)
+        {
+            return (a == b) || a.CompareTo(b) < 0;
+        }
+
+        public static bool operator >=(Fraction a, Fraction b)
+        {
+            return b <= a;
+        }
+
+        public static bool operator <(Fraction a, Fraction b)
+        {
+            return a.CompareTo(b) < 0;
+        }
+
+        public static bool operator >(Fraction a, Fraction b)
+        {
+            return b < a;
+        }
+    }
+}
---
+++ wGame/Core/Math.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace wGame.Core
+{
+    public static class Math
+    {
+        public static long Gcd(long a, long b)
+        {
+            if (a <= 0 || b <= 0)
+                throw new ArgumentException("gcd cannot be computed for nonpositive values");
+
+            while (a != 0 || b != 0)
+            {
+                if (a < b) b %= a;
+                else a %= b;
+            }
+
+            return a > 0 ? a : b;
+        }
+
+        public static long ModNormalize(long a, long mod)
+        {
+            if (mod < 2)
+                throw new ArgumentException("cannot perform modular operation for N < 2");
+
+            return ((a%mod) + a)%mod;
+        }
+
+        public static long ModAdd(long a, long b, long mod)
+        {
+            return ModNormalize(ModNormalize(a, mod) + ModNormalize(b, mod), mod);
+        }
+
+        public static long ModSub(long a, long b, long mod)
+        {
+            return ModAdd(a, -b, mod);
+        }
+
+        public static long ModMul(long a, long b, long mod)
+        {
+            return ModNormalize(ModNormalize(a, mod)*ModNormalize(b, mod), mod);
+        }
+    }
+}
